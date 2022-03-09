d9eba321194b3bdd9366571d003b0045978dedd5
IceKapper <71657151+IceKapper@users.noreply.github.com>
2022-03-09 17:24:22 +0000

    Completed Tutotial 6 (Refactoring)

--- SuperMarioBase/SuperMarioBase.cpp
+++ SuperMarioBase/SuperMarioBase.cpp
@@ -3,13 +3,16 @@
 #include <SDL_mixer.h>
 #include "constants.h"
 #include "Texture2D.h"
+#include "GameScreenManager.h"
 
 using namespace std;
 
 //globals
 SDL_Window* g_window = nullptr;
 SDL_Renderer* g_renderer = nullptr;
-Texture2D* g_texture = nullptr;
+GameScreenManager* game_screen_manager;
+Uint32 g_old_time;
+
 
 //Function prototypes
 bool InitSDL();
@@ -22,6 +25,11 @@ int main(int argc, char* args[])
 {
 	if (InitSDL())
 	{
+		game_screen_manager = new GameScreenManager(g_renderer, SCREEN_LEVEL1);
+		//set the time
+		g_old_time = SDL_GetTicks();
+
+
 		//flag to check if we wish to quit
 		bool quit = false;
 
@@ -74,13 +82,7 @@ bool InitSDL()
 				return false;
 			}
 
-			//Load the background texture
-			g_texture = new Texture2D(g_renderer);
 
-			if (!g_texture->LoadFromFile("Images/test.bmp"))
-			{
-				return false;
-			}
 
 		}
 		else
@@ -108,13 +110,16 @@ void CLoseSDL()
 	SDL_DestroyRenderer(g_renderer);
 	g_renderer = nullptr;
 
-	//release the texture
-	delete g_texture;
-	g_texture = nullptr;
+	//destroy the game screen manager
+	delete game_screen_manager;
+	game_screen_manager = nullptr;
+
 }
 
 bool Update() 
 {
+	Uint32 new_time = SDL_GetTicks();
+
 	//Event handler
 	SDL_Event e;
 	SDL_PollEvent(&e);
@@ -128,6 +133,8 @@ bool Update()
 			break;
 	}
 
+	game_screen_manager->Update((float)(new_time - g_old_time) / 1000.0f, e);
+	g_old_time = new_time;
 	return false;
 }
 
@@ -137,7 +144,7 @@ void Render()
 	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	SDL_RenderClear(g_renderer);
 
-	g_texture->Render(Vector2D(), SDL_FLIP_NONE);
+	game_screen_manager->Render();
 
 	//update the screen
 	SDL_RenderPresent(g_renderer);
